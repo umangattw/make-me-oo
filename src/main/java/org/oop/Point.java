eb674df2e06500ba7f19f95fe689ed8593fc3eb2
umangattw <umangsinghal@UMANGs-MacBook-Pro.local>
2021-01-17 02:27:33 +0530

    Point class is added

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,27 +1,23 @@
 package org.oop;
 
 public class Point {
-    private double x;
-    private double y;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
+    private final double x;
+    private final double y;
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public static double distance(Point from, Point to) {
+        return Math.hypot(difference(from.x, to.x), difference(from.y, to.y));
+    }
+
+    public static double direction(Point from, Point to){
+        return Math.atan2(difference(to.y, from.y), difference(to.x, from.x));
+    }
+
+    private static double difference(double pointOne, double pointTwo)
+    {
+        return pointOne - pointTwo;
     }
 }
